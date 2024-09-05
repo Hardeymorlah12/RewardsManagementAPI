20b4345bd542f109c03e1b353bafbc893d5658f3
Saheed Ademola Adewale <136191002+Hardeymorlah12@users.noreply.github.com>
2024-09-05 12:45:50 +0100

    Add files via upload

---
+++ RewardsManagementAPI/src/test/java/com/hardeymorlah/RewardsManagementAPI/RewardsManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hardeymorlah.RewardsManagementAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewardsManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
